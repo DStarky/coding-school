941e1c551586de2a66864b6870f6c4e82283239a
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-10-14 09:39:58 +0500

    menu settings to close

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './src/styles/style.scss'
 import { toggleMenu } from './src/scripts/toggleMenu';
+import { closeMenu } from './src/scripts/closeMenu';
 
 const menu = document.querySelector('.menu');
 const navigation = document.querySelector('.navigation');
@@ -10,3 +11,11 @@ const entryButton = document.querySelector('.header__button');
 menu.addEventListener('click', () => {
   toggleMenu(menu, navigation, entryButton)
 })
+
+document.addEventListener('click', function (event) {
+  if (!(navigation.contains(event.target) || menu.contains(event.target))) {
+    closeMenu(menu, navigation, entryButton);
+  } else if (event.target.tagName === 'A' || event.target.tagName === 'BUTTON' && navigation.contains(event.target)) {
+    closeMenu(menu, navigation, entryButton);
+  }
+});
---
+++ src/scripts/closeMenu.js
@@ -0,0 +1,5 @@
+export function closeMenu(button, menu, entry) {
+  button.classList.remove('change');
+  menu.classList.remove('change');
+  entry.classList.remove('change');
+}
